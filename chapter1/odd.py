199b858513b30605b2f5fb668d64de639694258c
EiChinn <eichinn77@gmail.com>
2018-03-05 11:19:29 +0800

    book's source and chapter1

---
+++ chapter1/odd.py
@@ -0,0 +1,8 @@
+from datetime import datetime;
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29,
+        31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+right_this_minutes = datetime.today().minute
+if right_this_minutes in odds:
+    print("This minute seems a little odd.")
+else:
+    print("Not an odd minute.") 
